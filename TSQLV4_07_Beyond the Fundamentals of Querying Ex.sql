3f2a5f569d1b59e9b74e73f7ac60b51fb1637b72
maplus <m-pluess@gmx.ch>
2019-01-17 21:41:17 +0100

    Ch07 finished

--- TSQLV4_07_Beyond the Fundamentals of Querying Ex.sql
+++ TSQLV4_07_Beyond the Fundamentals of Querying Ex.sql
@@ -1,10 +1,3 @@
----------------------------------------------------------------------
--- Microsoft SQL Server T-SQL Fundamentals
--- Chapter 07 - Beyond the Fundamentals of Querying
--- Exercises
--- © Itzik Ben-Gan 
----------------------------------------------------------------------
-
 -- All exercises for this chapter will involve querying the dbo.Orders
 -- table in the TSQLV4 database that you created and populated 
 -- earlier by running the code in Listing 7-1
@@ -28,6 +21,13 @@ C      10006       14          1                    1
 C      20002       20          2                    2
 C      30004       22          3                    3
 D      30007       30          1                    1
+
+select 
+	o.custid, o.orderid, o.qty,
+	RANK() over(partition by o.custid order by o.qty) rnk,
+	DENSE_RANK() over(partition by o.custid order by o.qty) drnk
+from dbo.orders o;
+
 
 -- 2
 -- The following query against the Sales.OrderValues view returns
@@ -61,6 +61,15 @@ val       rownum
 
 (795 row(s) affected)
 
+with CTE as (
+select distinct
+	val
+from Sales.OrderValues v
+)
+select val, ROW_NUMBER() over(order by val) rownum
+from CTE;
+
+
 -- 3
 -- Write a query against the dbo.Orders table that computes for each
 -- customer order:
@@ -84,6 +93,12 @@ C      10006       14          -8          -6
 C      20002       20          6           NULL
 D      30007       30          NULL        NULL
 
+select o.custid, o.orderid, o.qty, --o.orderdate,
+	o.qty - lag(o.qty) over(partition by o.custid order by o.orderdate) diffpref,
+	o.qty - lead(o.qty) over(partition by o.custid order by o.orderdate) diffpref
+from dbo.Orders o;
+
+
 -- 4
 -- Write a query against the dbo.Orders table that returns a row for each
 -- employee, a column for each order year, and the count of orders
@@ -96,6 +111,52 @@ empid       cnt2014     cnt2015     cnt2016
 1           1           1           1
 2           1           2           1
 3           2           0           2
+
+select x.empid,
+	sum(x.cnt2014) cnt2014,
+	sum(x.cnt2015) cnt2015,
+	sum(x.cnt2016) cnt2016
+from
+	(
+	select o.empid,
+		count(*) cnt2014,
+		0 cnt2015,
+		0 cnt2016
+	from dbo.Orders o
+	where o.orderdate >= cast('20140101' as date) and o.orderdate < cast('20150101' as date)
+	group by o.empid
+	union all
+	select o.empid,
+		0 cnt2014,
+		count(*) cnt2015,
+		0 cnt2016
+	from dbo.Orders o
+	where o.orderdate >= cast('20150101' as date) and o.orderdate < cast('20160101' as date)
+	group by o.empid
+	union all
+	select o.empid,
+		0 cnt2014,
+		0 cnt2015,
+		count(*) cnt2016
+	from dbo.Orders o
+	where o.orderdate >= cast('20160101' as date) and o.orderdate < cast('20170101' as date)
+	group by o.empid
+	) x
+group by x.empid;
+
+select
+	x.empid,
+	count(case when x.orderyear = 2014 then x.orderyear end) cnt2014,
+	count(case when x.orderyear = 2015 then x.orderyear end) cnt2015,
+	count(case when x.orderyear = 2016 then x.orderyear end) cnt2016
+from
+	(
+	select o.empid,
+		YEAR(o.orderdate) orderyear
+	from dbo.Orders o
+	) x
+group by x.empid;
+
 
 -- 5
 -- Run the following code to create and populate the EmpYearOrders table:
@@ -146,6 +207,15 @@ empid       orderyear   numorders
 3           2014        2
 3           2016        2
 
+select *
+from dbo.EmpYearOrders e;
+
+SELECT empid, cast(right(orderyear, 4) as int) orderyear, numorders
+FROM dbo.EmpYearOrders
+	UNPIVOT(numorders FOR orderyear IN(cnt2014, cnt2015, cnt2016)) AS U
+where numorders != 0;
+
+
 -- 6
 -- Write a query against the dbo.Orders table that returns the 
 -- total quantities for each:
@@ -190,6 +260,22 @@ groupingset empid       custid orderyear   sumqty
 2           3           NULL   2016        40
 
 (29 row(s) affected)
+
+select o.empid, o.custid, year(orderdate) orderyear, o.qty
+from dbo.Orders o;
+
+SELECT
+  GROUPING_ID(empid, custid, YEAR(Orderdate)) AS groupingset,
+  empid, custid, YEAR(Orderdate) AS orderyear, SUM(qty) AS sumqty
+FROM dbo.Orders
+GROUP BY
+  GROUPING SETS
+  (
+    (empid, custid, YEAR(orderdate)),
+    (empid, YEAR(orderdate)),
+    (custid, YEAR(orderdate))
+  );
+
 
 -- When you're done, run the following code for cleanup
 DROP TABLE IF EXISTS dbo.Orders;
